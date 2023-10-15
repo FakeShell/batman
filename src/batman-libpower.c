a1d48f41fc7d1743b6f14a9616ef19fd01c8ff43
Bardia Moshiri <fakeshell@bardia.tech>
2023-10-15 12:31:39 -0400

    batman-libpower: add support for powersave and performance hints

--- src/batman-libpower.c
+++ src/batman-libpower.c
@@ -4,16 +4,31 @@
 #include <stdio.h>
 #include <gbinder.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-// gcc libpower.c `pkg-config --libs --cflags libgbinder`
-void mode_setter(GBinderClient* client, const int enabled) {
+enum hints {
+    INTERACTION = 0x00000002,
+    POWERSAVE = 0x00000005,
+    PERFORMANCE = 0x00000006
+};
+
+void set_modes(GBinderClient* client, const int interactive, const enum hints hint) {
     int status;
     GBinderLocalRequest* req = gbinder_client_new_request(client);
     GBinderWriter writer;
 
+    // interactive mode
     gbinder_local_request_init_writer(req, &writer);
-    gbinder_writer_append_bool(&writer, enabled);
+    gbinder_writer_append_bool(&writer, interactive);
     gbinder_client_transact_sync_reply(client, 1, req, &status);
+    gbinder_local_request_unref(req);
+
+    // hints
+    req = gbinder_client_new_request(client);
+    gbinder_local_request_init_writer(req, &writer);
+    gbinder_writer_append_int32(&writer, (uint32_t) hint);
+    gbinder_writer_append_int32(&writer, interactive);
+    gbinder_client_transact_sync_reply(client, 2, req, &status);
     gbinder_local_request_unref(req);
 }
 
@@ -36,21 +51,23 @@ int main(int argc, char *argv[]) {
     }
 
     if (argc != 2) {
-        printf("Usage: %s <0|1> (0 for disabled, 1 for enabled)\n", argv[0]);
+        printf("Usage: %s <0|1> (0 for non-interactive + powersave, 1 for interactive + performance)\n", argv[0]);
         return 1;
     }
 
     int choice = atoi(argv[1]);
 
     if (choice == 0) {
-        mode_setter(client, 0);
+        set_modes(client, 0, POWERSAVE);
     } else if (choice == 1) {
-        mode_setter(client, 1);
+        set_modes(client, 1, PERFORMANCE);
     } else {
-        printf("Invalid argument. Use 0 to disable or 1 to enable interactive mode.\n");
+        printf("Invalid argument. Use 0 for non-interactive + powersave or 1 for interactive + performance.\n");
+        return 1;
     }
 
     gbinder_client_unref(client);
     gbinder_remote_object_unref(remote);
+
     return 0;
 }
