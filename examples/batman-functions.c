14f986814ce85d6668ef9e0e60e3d9cbb159112d
Bardia Moshiri <bardia@furilabs.com>
2024-11-14 18:41:27 -0500

    examples: redo with new functions and cleanup
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- examples/batman-functions.c
+++ examples/batman-functions.c
@@ -5,53 +5,47 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    UpClient *upower = up_client_new();
-    gdouble battery_percentage;
+    g_autoptr(UpClient) upower = NULL;
 
+    upower = up_client_new();
     if (upower == NULL) {
-        g_print("Could not connect to upower");
+        g_debug("Could not connect to upower");
         return 2;
     }
 
-    const gchar *statelabel = findBattery(upower, &battery_percentage);
+    gdouble battery_percentage = get_battery_percentage(upower);
+    batman_state_t battery_state = get_battery_state(upower);
+    const gchar *battery_status = get_battery_all(upower, NULL);
 
-    g_object_unref(upower);
+    double cpu = get_cpu_usage();
+    long double mem = mem_usage();
 
-    int cpu_usage = cpuUsage();
-    printf("CPU Usage: %d%%\n", cpu_usage);
+    printf("CPU Usage: %.1f%%\n", cpu);
+    printf("Memory Usage: %.1Lf%%\n", mem);
 
-    int mem_usage = memUsage();
-    printf("Memory Usage: %d%%\n", mem_usage);
-
-    if (check_batman_active() != -1) {
+    if (check_batman_active() != -1)
         printf("Batman active status: %s\n", bm_state.active ? "active" : "inactive");
-    } else {
+    else
         printf("Failed to check Batman active status\n");
-    }
 
-    if (check_batman_enabled() != -1) {
+    if (check_batman_enabled() != -1)
         printf("Batman enabled status: %s\n", bm_state.enabled ? "enabled" : "disabled");
-    } else {
+    else
         printf("Failed to check Batman enabled status\n");
-    }
 
-    int result = wlrdisplay(argc, argv);
-    printf("wlroots screen status: %s", result == 0 ? "yes\n" : "no\n");
+    int display_result = wlrdisplay(argc, argv);
+    printf("wlroots screen status: %s", display_result == 0 ? "yes\n" : "no\n");
 
-    if (statelabel != NULL) {
-        g_print("Battery status: %s\nBattery percentage: %.2f%%\n", statelabel, battery_percentage);
-    } else {
+    if (battery_state != BATMAN_NO_BATTERY && battery_state != BATMAN_UNKNOWN)
+        g_print("Battery status: %s\nBattery percentage: %.2f%%\n", battery_status, battery_percentage);
+    else
         g_print("No battery found\n");
-    }
 
-    gchar *state = waydroid_get_state();
-
-    if (state) {
-        g_print("Waydroid State: %s\n", state);
-        g_free(state);
-    } else {
+    g_autofree gchar *waydroid_state = waydroid_get_state();
+    if (waydroid_state)
+        g_print("Waydroid State: %s\n", waydroid_state);
+    else
         g_print("Failed to get Waydroid state.\n");
-    }
 
     return 0;
 }
