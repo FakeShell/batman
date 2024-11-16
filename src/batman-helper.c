8deb26bda035186af9f9f46c93e0093bebee38cf
Bardia Moshiri <bardia@furilabs.com>
2024-11-16 11:31:30 -0500

    batman-helper: add waydroid_app_open to helpers
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- src/batman-helper.c
+++ src/batman-helper.c
@@ -7,7 +7,7 @@
 #include "wlrdisplay.h"
 #include "getinfo.h"
 
-static const char* USAGE = "[cpu|mem|wlrdisplay|battery|battery_percentage|battery_state|batman_active|batman_enabled|waydroid_state|waydroid_screen]";
+static const char* USAGE = "[cpu|mem|wlrdisplay|battery|battery_percentage|battery_state|batman_active|batman_enabled|waydroid_state|waydroid_screen|waydroid_app_open]";
 
 static void print_usage(const char* program_name) {
     printf("Usage: %s %s\n", program_name, USAGE);
@@ -25,7 +25,8 @@ static const char* get_battery_state_str(batman_state_t state) {
             return "fully-charged";
         case BATMAN_UNKNOWN:
             return "unknown";
-        default: return "invalid state";
+        default:
+            return "invalid state";
     }
 }
 
@@ -108,6 +109,12 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[1], "waydroid_screen") == 0) {
         gboolean is_asleep = waydroid_screen_status();
         g_print("%s\n", is_asleep ? "False" : "True");
+        return EXIT_SUCCESS;
+    }
+
+    if (strcmp(argv[1], "waydroid_app_open") == 0) {
+        gboolean app_open = waydroid_app_open();
+        g_print("%s\n", app_open ? "False" : "True");
         return EXIT_SUCCESS;
     }
 
